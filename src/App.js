688692f6a7bdc0d039ee249a09a725e5a95921bd
Anand Sunderrajan <36977554+Xeno-404@users.noreply.github.com>
2020-06-18 10:29:04 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import 'roboto-npm-webfont';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import ContactComponent from "./sections/contacts/contacts";
+import LanguagesComponent from "./sections/programming/programming";
+import FrameworksComponent from "./sections/frameworks/frameworks";
+import LandingComponent from "./sections/landing/landing";
+import ProjectsComponent from "./sections/projects/projects";
+import IntroComponent from "./sections/intro/intro";
+import WebsiteComponent from "./sections/websitegit/websitegit";
 
-export default App;
