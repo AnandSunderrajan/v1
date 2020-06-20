528141840a5359e20f12f80dd183c4b09c399871
Anand Sunderrajan <36977554+Xeno-404@users.noreply.github.com>
2020-06-19 18:46:29 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,3 +11,52 @@ import ProjectsComponent from "./sections/projects/projects";
 import IntroComponent from "./sections/intro/intro";
 import WebsiteComponent from "./sections/websitegit/websitegit";
 
+
+class App extends Component {
+  render() {
+    return (
+        <MuiThemeProvider>
+          <div className="App">
+            <div className="section header-section">
+              <div className="section-container header-component">
+                <LandingComponent></LandingComponent>
+              </div>
+            </div>
+            <div className="section colored">
+              <div className="section-container">
+                <IntroComponent></IntroComponent>
+              </div>
+            </div>
+            <div className="section">
+              <div className="section-container">
+                <ProjectsComponent></ProjectsComponent>
+              </div>
+            </div>
+            <div className="section colored">
+              <div className="section-container">
+                <LanguagesComponent></LanguagesComponent>
+              </div>
+            </div>
+            <div className="section">
+              <div className="section-container">
+                <FrameworksComponent></FrameworksComponent>
+              </div>
+            </div>
+            <div className="section">
+              <div className="section-container">
+                <WebsiteComponent></WebsiteComponent>
+              </div>
+            </div>
+            <div className="section colored">
+              <div className="section-container">
+                <ContactComponent></ContactComponent>
+                <MyForm/>
+              </div>
+            </div>
+          </div>
+        </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
