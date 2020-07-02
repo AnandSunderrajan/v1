ec893e8559f89f985f846b315897a1dda0a23c8d
Anand Sunderrajan <36977554+Xeno-404@users.noreply.github.com>
2020-07-02 16:00:00 -0600

    Update intro.jsx

--- src/sections/intro/intro.jsx
+++ src/sections/intro/intro.jsx
@@ -11,7 +11,7 @@ class IntroComponent extends Component {
                         <a className={"name"}> Anand</a></p>
                 </div>
                 <p className="introduction-text">
-                    A university student living in IL, US. I'm currently pursuing a B.Sc. in Computer Engineering from the University of Illinois at Urbana-Champaign.
+                    A motivated young professional with an amalgam of unique experiences looking to further knowledge and experience while contributing to society. Currently pursuing a B.Sc. in Computer Engineering from the University of Illinois at Urbana-Champaign.
                 </p>
                 <br/>
                 <Button target={"_blank"} href={"https://github.com/AnandSunderrajan/anandsunderrajan.github.io/raw/master/src/sections/documents/Anand_Sunderrajan_Resume.pdf"} style={{color: 'white'}}>My Resume</Button>
